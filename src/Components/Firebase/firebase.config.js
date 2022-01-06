0e51166f1b50c025fb26cdcbd721b4f8e00e2f70
unknown <web.faisal.bd@gmail.com>
2022-01-06 21:46:48 +0600

    .env.local file added

--- src/Components/Firebase/firebase.config.js
+++ src/Components/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyACl3LaTKm7GScWt0XH5WzJG5hiaGNMT1Y",
-    authDomain: "happy-shopping-mern.firebaseapp.com",
-    projectId: "happy-shopping-mern",
-    storageBucket: "happy-shopping-mern.appspot.com",
-    messagingSenderId: "500834036803",
-    appId: "1:500834036803:web:1f56e19dac4f73dac7f8f6"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
   };
 
   export default firebaseConfig;
